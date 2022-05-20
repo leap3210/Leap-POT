856b869d970af856f75cbce6052c724e6c7599cf
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-05-20 11:27:09 +0100

    Basic NFT contract

---
+++ SolidityContracts/contracts/LeapNFT.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
+
+/// @custom:security-contact security-leap@mail.com
+contract ListenToEarnProtocol is ERC20, ERC20Burnable, Pausable, AccessControl {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("Listen to Earn Protocol", "LPT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    // Burn NFT with pedge return
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
