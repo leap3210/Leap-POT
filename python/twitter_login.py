d97b75cce7fbe3494a9470f7b32e7861693c6f32
stinkerz420 <49249923+stinkerz420@users.noreply.github.com>
2022-05-19 22:53:41 -0400

    added python POST method and node server to receive

--- python/twitter_login.py
+++ python/twitter_login.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import credentials
 import time
+import json
+import requests
+import string
 
 driverLocation = r"C:\development\downloads\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(driverLocation)
@@ -28,7 +31,7 @@ time.sleep(2)
 password.send_keys(credentials.password)
 password.send_keys(Keys.RETURN)
 
-twitterSpace = "https://twitter.com/i/spaces/1dRKZlOEzOQJB"
+twitterSpace = "https://twitter.com/i/spaces/1OwGWzOYdDpKQ?"
 getTwitterSpace = driver.get(twitterSpace)
 print('twitter space joined')
 time.sleep(2)
@@ -47,24 +50,29 @@ spaceUsers = driver.find_elements(by=By.XPATH, value=f'//*[@id="layers"]/div[2]/
 
 userTime = []
 userDict = {}
+result = [{'host': 1234}]
 
-
-
-while 1:
+#while(1)
+for x in range(2):
     # We should check for an element that we expect to be on the page here to make sure we haven't timed out
     #   If we have timed out, redo the login function
     listOfNames = [] # List of names to store
-
+    time.sleep(1)
+    shouldbreak = False
     for i in range(2,1000): # Loop up to 1000 rows
+        if shouldbreak:
+            break
         for j in range(1,5): # Loop up to 3 columns (3 show up in my browser)
             try:
                 # Try to get the name and add it to the list
+                #time.sleep(1)
                 name = driver.find_element(by=By.XPATH, value=f'/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[1]/div/div/div[2]/div/div/div[' + str(i) + ']/div/div[' + str(j) + ']/div[2]/div/div/div/div/span[1]/span/span[1]').get_attribute('innerHTML')
+                #name = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, f'/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[1]/div/div/div[2]/div/div/div[' + str(i) + ']/div/div[' + str(j) + ']/div[2]/div/div/div/div/span[1]/span/span[1]').get_attribute('innerHTML')))
                 listOfNames.append(name)
             except:
                 # If we hit an exception because it didn't exist then...
-                i = 1001 # Set our counter variables so we leave the loops
-                j = 4
+                #i = 1001 # Set our counter variables so we leave the loops
+                #j = 6
                 # here you would post the names I guess to the API, just printing for now
                 for name in listOfNames:
                     if name in userDict.keys():
@@ -75,13 +83,32 @@ while 1:
 
                 print('\n\n')
                 print(userDict)
-                
-                time.sleep(10) # sleep for some amount of time and check the page again for an updated list. Might have to reload?
+                shouldbreak = True
+                break
+                #time.sleep(15) # sleep for some amount of time and check the page again for an updated list. Might have to reload?
                 #driver.refresh()
                 #time.sleep(5)
                 #driver.find_element(by=By.XPATH, value=f'//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div/div/div[4]/div/div').click()
 
 
-time.sleep(4)
 
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root'")))
+for k, v in userDict.items():
+    result.append({'name': k, 'timeWatched': v})
+
+print(json.dumps(result))
+
+
+url = "http://localhost:8081/addUser"
+
+headers = {
+    'content-type': "application/json",
+    'cache-control': "no-cache",
+    }
+    
+
+response = requests.request("POST", url, json=result, headers=headers)
+
+print(response.text)
+
+#print(spaceJoin)
+#WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, f'/html/body/div[1]/div/div/div[1]/div/div[1]/div/div/div/div[1]/div/div/div[2]/div/div/div[' + str(i) + ']/div/div[' + str(j) + ']/div[2]/div/div/div/div/span[1]/span/span[1]').get_attribute('innerHTML'))))
