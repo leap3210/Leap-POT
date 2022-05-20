b769b16b020978f1dc85004230da673e63b5be57
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-05-20 16:05:57 +0100

    Listeners structure

--- SolidityContracts/contracts/LeapUserRegistry.sol
+++ SolidityContracts/contracts/LeapUserRegistry.sol
@@ -6,6 +6,19 @@ import "hardhat/console.sol";
 /// @custom:security-contact security-leap@mail.com
 contract LeapUserRegistry {
 
+    mapping (address -> Listener);
+    
+    struct Listener {
+        // LEAP Token (ERC20) reward multiplayer. Default value 1.
+        uint256 leapRewardMultiplier;
+        // Unclaimed, Accumulated LEAP Token rewards
+        uint256 leapRewards;
+        // Last time rewards were calculated on chain
+        uint256 lastCheckpoint;
+        // LEAP NFT ID's ownership
+        uint256[] leapNFTs;
+    }
+
     // Register host (Stake NFT)
 
     // Register host event / project (Twitter Space)
