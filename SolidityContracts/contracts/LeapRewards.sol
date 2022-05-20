05e5a660c30f2ce141d5a8d3ba6a7bd0c5950510
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-05-20 15:18:13 +0100

    Logic description for Reward calculation and distribution

---
+++ SolidityContracts/contracts/LeapRewards.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @custom:security-contact security-leap@mail.com
+contract LeapRewards {
+    
+    // Calculate reward values according to RewardPool of a Host Event and his Listeners contribution
+
+    // Transfer reward assets from RewardPool into Superfluid contract
+
+    // Set recipients and amounts to recieve for Superfluid contract
+
+    // Update timeToPay for staked NFT owners (Listeners) on LeapHub.sol contract
+    // Set timeToPay to 0
+    // Set lastTimePaid to current blockstamp
+    
+}
