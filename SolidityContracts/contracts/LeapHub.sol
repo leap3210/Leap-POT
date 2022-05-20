372bc87215589c3e27b3ccf885265138df15b53d
Loizage <loizage@Loizages-MacBook-Pro.local>
2022-05-20 15:20:16 +0100

    Logic description for main Leap Protocol contract (Hub)

---
+++ SolidityContracts/contracts/LeapHub.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @custom:security-contact security-leap@mail.com
+contract LeapHub {
+  
+    // Stake LeapNFT
+
+    // Unstake LeapNFT
+
+    // Get Owners list of staked LeapNFTs
+
+    // Whitelist of allowed NFT contracts to stake
+
+    // Update timeToPay for staked NFT owners (Listeners)
+
+}
