c07708a0d9455f898148f4e52701a93172c2ee2c
stinkerz420 <49249923+stinkerz420@users.noreply.github.com>
2022-05-22 14:36:52 -0400

    uploading used sponsor tech code

---
+++ node/web3-server.js
@@ -0,0 +1,82 @@
+// var express = require('express');
+var Web3 = require('web3');
+const Provider = require('@truffle/hdwallet-provider');
+
+var SmartContractAddress = "0x34239e0300Fc19881e8F1b867681741D02850024";
+var SmartContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "newValue",
+				"type": "string"
+			}
+		],
+		"name": "pushToRow",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "store",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getRow",
+		"outputs": [
+			{
+				"internalType": "string[]",
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "retrieve",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "row",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+var address = "";
+var privatekey = "";
+
+var rpcurl = "";
